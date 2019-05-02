c7a4f6410817877d49f963964dd2141754b6b926
Yanis <yanis.urbis@gmail.com>
2019-05-02 11:27:50 +0300

    Add in memory storage implementation

--- src/link_shortener/core.clj
+++ src/link_shortener/core.clj
@@ -69,6 +69,44 @@
           (testing "equal to the links we created"
             (is (= id-urls links))))))))
 
+(defn create-link*
+  [!stg id url]
+  (when-not (contains? @!stg id)
+    (swap! !stg assoc id url)
+    id))
+
+(defn get-link*
+  [!stg id]
+  (get @!stg id))
+
+(defn update-link*
+  [!stg id new-url]
+  (when (contains? @!stg id)
+    (swap! !stg assoc id new-url)
+    id))
+
+(defn delete-link*
+  [!stg id]
+  (swap! !stg dissoc id)
+  nil)
+
+(defn list-links*
+  [!stg]
+  @!stg)
+
+(defn in-memory-storage
+  []
+  (let [!stg (atom {})]
+    (reify Storage
+      (create-link [_ id url] (create-link* !stg id url))
+      (get-link [_ id] (get-link* !stg id))
+      (update-link [_ id url] (update-link* !stg id url))
+      (delete-link [_ id] (delete-link* !stg id))
+      (list-links [_] (list-links* !stg)))))
+
+(deftest in-memory-storage-test
+  (let [stg (in-memory-storage)]
+    (is-valid-storage stg)))
 
 (stop-server)
 (reset! server (s/run-server app {:port 8080}))
