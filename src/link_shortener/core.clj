1df0cbfbc847aae57604ae834442cc40cc02f8dc
Yanis <yanis.urbis@gmail.com>
2019-05-02 09:55:09 +0300

    Add testing function for storage

--- src/link_shortener/core.clj
+++ src/link_shortener/core.clj
@@ -27,5 +27,48 @@
                  :status 200
                  :body "Hello yan")))
 
+(defprotocol Storage
+  (create-link [this id url])
+  (get-link [this id])
+  (update-link [this id new-url])
+  (delete-link [this id])
+  (list-links [this]))
+
+(defn is-valid-storage
+  [stg]
+  (let [url "http://example.com"
+        id "book"]
+    (testing "can store and retrieve a link"
+      (testing "create-link returns the id"
+        (is (= id (create-link stg id url)))
+
+        (testing "and it won't overwrite an existing id"
+          (is (nil? (create-link stg id "bogus")))
+          (is (= url (get-link stg id))))))
+
+    (testing "can update a link"
+      (let [new-url "http://new.example.com"]
+        (update-link stg id new-url)
+        (is (= new-url (get-link stg id)))))
+
+    (testing "can delete a link"
+      (delete-link stg id)
+      (is (nil? (get-link stg id))))
+
+    (testing "can list all links"
+      (let [id-urls {"a" "http://example.com/a"
+                     "b" "http://example.com/b"
+                     "c" "http://example.com/c"}
+            ids (doseq [[id url] id-urls]
+                  (create-link stg id url))
+            links (list-links stg)]
+
+        (testing "in a map"
+          (is (map? links))
+
+          (testing "equal to the links we created"
+            (is (= id-urls links))))))))
+
+
 (stop-server)
 (reset! server (s/run-server app {:port 8080}))
