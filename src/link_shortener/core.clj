537e172575a7f269b69d15e1b7c7dbd7fd48dc1c
Yanis <yanis.urbis@gmail.com>
2019-05-08 10:27:37 +0300

    Add all other handlers

--- src/link_shortener/core.clj
+++ src/link_shortener/core.clj
@@ -19,18 +19,6 @@
             [link-shortener.handlers :as handlers])
   (:gen-class))
 
-(comment (defn create-app []
-           (let [stg (in-memory-storage)]
-             (routes
-               (POST "/links" {{id "id" url "url"} :params} (create-link stg id url))
-               (GET "/links/:id" [id] (get-link stg id))
-               (PUT "/links/:id" [id :as {{url "url"} :params}] (update-link stg id url))
-               (DELETE "/links/:id" [id] (delete-link stg id))
-               (GET "/links" [] (-> (list-links stg)
-                                    wrap-json-response))
-               (route/not-found "Not Found")))))
-
-
 (s/def ::url (s/and spec/string? is-valid-url?))
 (s/def ::id spec/string?)
 (s/def ::success-link spec/string?)
@@ -38,6 +26,9 @@
 (s/def ::links spec/int?)
 
 (def stg (in-memory-storage))
+
+;; TODO not found
+;; TODO more security with specs
 
 (def app
   (api
@@ -51,50 +42,25 @@
     (context "/links" []
       (context "/:id" [id]
         (resource {:coercion :spec
-                   :get      {:handler   (fn [_]
-                                           (println "OK")
-                                           (ok id))}}))
+                   :get      {:parameters {:params (s/keys :req-un [::id])}
+                              :handler (fn [_]
+                                         (handlers/get-link stg id))}
+                   :put {:parameters {:params (s/keys :req-un [::id ::url])}
+                         :handler (fn [{{:keys [id url]} :params}]
+                                    (handlers/update-link stg id url))}
+                   :delete {:parameters {:params (s/keys :req-un [::id])}
+                            :handler (fn [_]
+                                       (handlers/delete-link stg id))}}))
       (resource
         {:coercion :spec
          :post     {:parameters {:form-params (s/keys :req-un [::id ::url])}
                     :responses  {200 {:schema ::success-link}
                                  404 {:schema ::error-explanation}}
                     :handler    (fn [params]
-                                  (let [{{:keys [id url]} :params} params
-                                        _ (clojure.pprint/pprint params)]
+                                  (let [{{:keys [id url]} :params} params]
                                     (handlers/create-link stg id url)))}
-         :get      {:handler   (fn [_]
-                                (handlers/list-links stg))}}))))
-
-(def app* (wrap-json-response app))
-
-(comment (def old-app
-           (api
-             {:coercion :spec
-              :swagger
-                        {:ui   "/"
-                         :spec "/swagger.json"
-                         :data {:info {:title       "Link shortener"
-                                       :description "Compojure Api example"}
-                                :tags [{:name "api", :description "some apis"}]}}}
-
-             (context "/math/:a" []
-               :path-params [a :- ::a]
-
-               (sweet/POST "/plus" []
-                 :query-params [b :- ::b, {c :- ::c 0}]
-                 :body [numbers (s/keys :req-un [::d])]
-                 :return (s/keys :req-un [::total])
-                 (ok {:total (+ a b c (:d numbers))})))
-
-             (context "/data-math" []
-               (resource
-                 ;; to make coercion explicit
-                 {:coercion :spec
-                  :get      {:parameters {:query-params (s/keys :req-un [::x ::y])}
-                             :responses  {200 {:schema ::total-body}}
-                             :handler    (fn [{{:keys [x y]} :query-params}]
-                                           (ok {:total (+ x y)}))}})))))
+         :get      {:handler (fn [_]
+                               (handlers/list-links stg))}}))))
 
 (defn get-resp [req]
   (-> (app req)
@@ -102,15 +68,19 @@
       slurp
       (cheshire/parse-string true)))
 
+(defn get-resp1 [req]
+  (-> (app req)
+      :body))
+
 (let [req1 (-> (mock/request :post "/links")
                (mock/body {:id "google" :url "http://www.google.com"})
                (mock/content-type "application/x-www-form-urlencoded"))
       req2 (-> (mock/request :get "/links"))
-      req3 (-> (mock/request :get "/links/123"))]
+      req3 (-> (mock/request :get "/links/google"))]
   (do
     (println (get-resp req1))
-    (println (get-resp req2))
-    (println (get-resp req3))))
+    ;(println (get-resp req2))
+    (println (get-resp1 req3))))
 
 (defonce server (atom nil))
 
